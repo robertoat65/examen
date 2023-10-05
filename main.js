47a5554c522e0e0c27e8cc05d2519a8ce15efbce
robertoat65 <ratilano@ucol.mx>
2023-10-04 18:58:29 -0600

    segunda vt

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let resultado
 let materia
 
-materia == "Introduccion a la programacion";
-resultado == 2*2;
+materia = "Introduccion a la programacion";
+resultado = 2 * 2;
 
 console.log(resultado);
-console.log(materia)
+console.log(materia);
